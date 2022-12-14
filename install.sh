12c746c423345b48af463c365e05f4dfb01e54eb
Kevin Evans <kwevans@gmail.com>
2022-12-14 18:59:22 +0000

    add install script

---
+++ install.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env sh
+
+
+DOTFILES="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+# ln -sf $DOTFILES/zsh/zshrc $HOME/.zshrc
+
+rm -rf $HOME/.config/nvim
+ln -s $DOTFILES/nvim $HOME/.config/nvim
+
+ln -sf $DOTFILES/tmux/tmux.conf $HOME/.tmux.conf
+
+mkdir -p $HOME/.local/bin
+
+ln -sf $DOTFILES/scripts/t $HOME/.local/bin/t
+ln -sf $DOTFILES/scripts/deliver $HOME/.local/bin/deliver
+
+mkdir -p $HOME/.nvm
+ln -sf $DOTFILES/nvm/default-packages $HOME/.nvm/default-packages
